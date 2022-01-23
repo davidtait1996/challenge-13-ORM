a2e044b4960044d095f8b87d5b8136ddc81cc3ca
David "Smoom" Tait <david.tait.96@gmail.com>
2022-01-23 16:07:07 -0700

    finished relationships

--- models/index.js
+++ models/index.js
@@ -18,14 +18,14 @@ Category.hasMany(Product, {
 Product.belongsToMany(Tag, {
   through: ProductTag,
   as: 'product_tags',
-  foreignKey: ''
+  foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   as: 'tagged_products',
-  foreignKey: ''
+  foreignKey: 'tag_id'
 })
 
 module.exports = {
